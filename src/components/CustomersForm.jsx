7bb091d29d34a0818355d03cb3a1e2c5fafc1ee4
AdrianOjeda <adrian.16300022@gmail.com>
2024-10-19 15:56:51 -0600

    List clientes in frontend done

--- src/components/CustomersForm.jsx
+++ src/components/CustomersForm.jsx
@@ -1,80 +1,116 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import InputForm from './InputForm';
 import { useNavigate } from "react-router-dom";
 
 function CustomersForm() {
     
     const [formData, setFormData] = useState({
-        nombre: '',
-        apellidoPaterno: '',
-        apellidoMaterno: '',
-    })
+        firstName: '',
+        lastName: '',
+        phoneNumber: '',
+        address: '',
+    });
 
     const navigate = useNavigate();
 
-    const handleSubmit=(e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        console.log(formData)
+        console.log(formData);
 
-        navigate('/customers', {
-            replace: true,
-            state: { formData }
-        });
+        try {
+            
+            const response = await fetch("/api/clientes",{
+                method: 'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+    
+            if(response.ok){
 
+                swal({icon:'success', title:'Cliente agregado con exito'});
+                navigate('/customers', {
+                    replace: true,
+                    state: { formData }
+                });
+        
+                setFormData({
+                    firstName: '',
+                    lastName: '',
+                    phoneNumber: '',
+                    address: '',
+                });
+            }else{
+                
+            }
+        } catch (error) {
+            console.error(error);
+            
+        }
+        
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
         setFormData({
-            nombre: '',
-            apellidoPaterno: '',
-            apellidoMaterno: '',
-        });
-    }
-        const handleInputChange = (e) => {
-            const { name, value } = e.target;
-            setFormData({
-                ...formData,
+            ...formData,
             [name]: value,
         });
     };
 
-    return(
+    return (
         <form
             className="w-full mx-auto p-6 bg-white dark:bg-slate-800 rounded-lg shadow-md"
-            onSubmit={handleSubmit} 
+            onSubmit={handleSubmit}
         >
             <div className="space-y-4">
                 
                 <div>
-                    <label htmlFor="name" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Nombre</label>
+                    <label htmlFor="firstName" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Nombre</label>
                     <InputForm
-                        id="nombre"
+                        id="firstName"
                         type="text"
-                        placeholder="Ingrese el nombre del cliente"
-                        name="nombre"
-                        value={formData.nombre}
-                        onChange={handleInputChange} 
+                        placeholder="Ingresa el nombre"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleInputChange}
                         className="mt-1 block w-full rounded-md shadow-sm dark:bg-slate-700 h-10 p-1 dark:text-white"
                     />
                 </div>
                 <div>
-                    <label htmlFor="lastName" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Apellido Paterno</label>
+                    <label htmlFor="lastName" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Apellidos</label>
                     <InputForm
-                        id="apellidoPaterno"
+                        id="lastName"
                         type="text"
-                        placeholder="Ingrese el Apellido Paterno del cliente"
-                        name="apellidoPaterno"
-                        value={formData.apellidoPaterno}
-                        onChange={handleInputChange} 
+                        placeholder="Ingresa los apellidos"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleInputChange}
                         className="mt-1 block w-full rounded-md shadow-sm dark:bg-slate-700 h-10 p-1 dark:text-white"
                     />
                 </div>
                 <div>
-                    <label htmlFor="lastName" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Apellido Materno</label>
+                    <label htmlFor="phoneNumber" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Teléfono</label>
                     <InputForm
-                        id="apellidoMaterno"
+                        id="phoneNumber"
+                        type="tel"
+                        placeholder="Ingresa el telefono del cliente"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleInputChange}
+                        className="mt-1 block w-full rounded-md shadow-sm dark:bg-slate-700 h-10 p-1 dark:text-white"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="address" className="block text-lg font-medium text-gray-800 dark:text-slate-200">Dirección</label>
+                    <InputForm
+                        id="address"
                         type="text"
-                        placeholder="Ingrese el Apellido Materno del cliente"
-                        name="apellidoMaterno"
-                        value={formData.apellidoMaterno}
-                        onChange={handleInputChange} 
+                        placeholder="Ingresa la direccion del cliente"
+                        name="address"
+                        value={formData.address}
+                        onChange={handleInputChange}
                         className="mt-1 block w-full rounded-md shadow-sm dark:bg-slate-700 h-10 p-1 dark:text-white"
                     />
                 </div>
@@ -84,10 +120,10 @@ function CustomersForm() {
                 type="submit"
                 className="mt-6 w-full px-4 py-2 bg-slate-900 text-white rounded-md hover:border-b-indigo-950 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-                Enviar
+                Submit
             </button>
         </form>
-    )
+    );
 }
 
 export default CustomersForm;
--- src/components/ListCustomers.jsx
+++ src/components/ListCustomers.jsx
@@ -1,43 +1,56 @@
 import React, { useEffect, useState } from "react";
 import InputForm from "./InputForm";
 import Table from "./Table";
-import MobileTable from "./MobileTable"
+import MobileTable from "./MobileTable";
 import { useNavigate } from "react-router-dom";
 
 function ListCustomers() {
     const headers = [
         { label: 'ID', key: 'id' },
-        { label: 'Nombre', key: 'nombre' },
-        { label: 'Primer Apellido', key: 'primer_apellido' },
-        { label: 'Segundo Apellido', key: 'segundo_apellido' },
+        { label: 'Nombre', key: 'firstName' },
+        { label: 'Apellido', key: 'lastName' },
+        { label: 'Dirección', key: 'address' },
+        { label: 'Teléfono', key: 'phone' },
         { label: 'Editar', key: 'editar' }, // Para editar
         { label: 'Eliminar', key: 'eliminar' } // Para eliminar
     ];
+
     const [datas, setDatas] = useState([]);
     const [isEditPopupOpen, setIsEditPopupOpen] = useState(false);
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
-        nombre: '',
-        primer_apellido: '',
-        segundo_apellido: '',
+        firstName: '',
+        lastName: '',
+        address: '',
+        phone: ''
     });
- // Simulando la carga de datos de una API
+
     useEffect(() => {
-
-        const fetchData = async () => {
-            const response = [
-                { id: 1, nombre: 'Jhovany', primer_apellido: 'Flores', segundo_apellido: 'Lozano',}
-                
-            ];
-            setDatas(response);
-        };
-
+        
         fetchData();
-    }, []);
+    }, [location.state]);
+
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("/api/clientes");
+            if(response.ok){
+                const data = await response.json();
+                console.log(data);
+                setDatas(data);
+            }else{
+                throw new Error("Couldn't retrieve customers list");
+            }
+            
+        } catch (error) {
+            console.error("Error fetching data", error);
+            
+        }
+    };
 
     const openEditPopup = (id) => {
-        setFormData(id)
-        console.log(id)
+        setFormData(id);
+        console.log(id);
         setIsEditPopupOpen(true);
     };
 
@@ -48,8 +61,8 @@ function ListCustomers() {
     const handleEditSubmit = (e) => {
         e.preventDefault();
         console.log(formData);
-        //aqui debe ir la api que edita
-        
+        // API to edit should go here
+
         closeEditPopup();
 
         navigate('/customers', {
@@ -58,15 +71,15 @@ function ListCustomers() {
         });
 
         setFormData({
-            nombre: '',
-            apellidoPaterno: '',
-            apellidoMaterno: '',
+            firstName: '',
+            lastName: '',
+            address: '',
+            phone: ''
         });
     };
 
     const handleDeleteSubmit = (id) => {
         console.log(`Eliminando cliente con ID: ${id}`);
-    
     };
 
     const handleInputChange = (e) => {
@@ -75,7 +88,7 @@ function ListCustomers() {
             ...formData,
             [name]: value,
         });
-    }
+    };
 
     return (
         <div className="h-auto">
@@ -84,83 +97,94 @@ function ListCustomers() {
                     <h3 className="text-3xl font-medium text-gray-700 dark:text-white">Clientes</h3>
                     <div className="flex flex-col mt-8">
                         <Table headers={headers} datas={datas} openEditPopup={openEditPopup} handleDeleteSubmit={handleDeleteSubmit} />
-                        <MobileTable  datas={datas} handleDeleteSubmit={handleDeleteSubmit} openEditPopup={openEditPopup} type="cliente"/>
+                        <MobileTable datas={datas} handleDeleteSubmit={handleDeleteSubmit} openEditPopup={openEditPopup} type="cliente" />
                     </div>
-                    
                 </div>
             </section>
             {isEditPopupOpen && (
-            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-auto w-full flex justify-center items-center" id="my-modal">
-                <div className="relative mx-auto p-5 border w-full max-w-lg shadow-lg rounded-md bg-white dark:bg-gray-800">
-                    <div className="mt-3 text-center">
-                        <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">Editar Cliente</h3>
-                        <form onSubmit={handleEditSubmit} className="mt-2 text-left">
-                            <div className="mb-4">
-                                <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="name">
-                                    Nombre
-                                </label>
-                                <InputForm 
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline" 
-                                    id="nombre" 
-                                    type="text" 
-                                    name="nombre"
-                                    placeholder="Ingrese el nuevo Nombre del cliente"
-                                    value={formData.nombre}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="model">
-                                    Apellido Paterno
-                                </label>
-                                <InputForm 
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline" 
-                                    id="primer_apellido" 
-                                    type="text" 
-                                    name="primer_apellido"
-                                    placeholder="Ingrese el nuevo Apellido Materno del cliente"
-                                    value={formData.primer_apellido}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="matriculate">
-                                    Apellido Materno
-                                </label>
-                                <InputForm 
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline" 
-                                    id="segundo_apellido" 
-                                    type="text" 
-                                    name="segundo_apellido"
-                                    placeholder="Ingrese el Nuevo Apellido Materno del cliente"
-                                    value={formData.segundo_apellido}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                           
-                            <div className="flex items-center justify-between">
-                            <button 
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-                                type="submit"
-                                id="saveChange"
-                                name="rol"
-                            >
-                                Guardar Cambios
-                            </button>
-                            <button 
-                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-                                type="button"
-                                onClick={closeEditPopup}
-                            >
-                                Cancelar
-                            </button>
+                <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-auto w-full flex justify-center items-center" id="my-modal">
+                    <div className="relative mx-auto p-5 border w-full max-w-lg shadow-lg rounded-md bg-white dark:bg-gray-800">
+                        <div className="mt-3 text-center">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">Editar Cliente</h3>
+                            <form onSubmit={handleEditSubmit} className="mt-2 text-left">
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="firstName">
+                                        Nombre
+                                    </label>
+                                    <InputForm
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                                        id="firstName"
+                                        type="text"
+                                        name="firstName"
+                                        placeholder="Ingrese el nuevo Nombre del cliente"
+                                        value={formData.firstName}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="lastName">
+                                        Apellido
+                                    </label>
+                                    <InputForm
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                                        id="lastName"
+                                        type="text"
+                                        name="lastName"
+                                        placeholder="Ingrese el nuevo Apellido del cliente"
+                                        value={formData.lastName}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="address">
+                                        Dirección
+                                    </label>
+                                    <InputForm
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                                        id="address"
+                                        type="text"
+                                        name="address"
+                                        placeholder="Ingrese la nueva Dirección del cliente"
+                                        value={formData.address}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="phone">
+                                        Teléfono
+                                    </label>
+                                    <InputForm
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:bg-gray-900 dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+                                        id="phone"
+                                        type="text"
+                                        name="phone"
+                                        placeholder="Ingrese el nuevo Teléfono del cliente"
+                                        value={formData.phone}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
 
-                            </div>
-                        </form>
+                                <div className="flex items-center justify-between">
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                        type="submit"
+                                        id="saveChange"
+                                    >
+                                        Guardar Cambios
+                                    </button>
+                                    <button
+                                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                        type="button"
+                                        onClick={closeEditPopup}
+                                    >
+                                        Cancelar
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-)}
+            )}
         </div>
     );
 }
