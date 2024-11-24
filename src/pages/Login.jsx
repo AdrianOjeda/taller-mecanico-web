2e0fe11e215696c12989f31c663dd773fd7a119f
AdrianOjeda <adrian.16300022@gmail.com>
2024-11-23 18:26:54 -0600

    Login fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,10 +26,10 @@ function Login(){
         event.preventDefault();
         console.log(formData);
         
-        const response = await fetch('https://taller-backend-prod-production.up.railway.app/taller-app/users', {
-            method: 'GET',
+        const response = await fetch('https://taller-backend-prod-production.up.railway.app/taller-app/users/login', {
+            method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            //body: JSON.stringify(formData),
+            body: JSON.stringify(formData),
             credentials: 'include',
         });
     
